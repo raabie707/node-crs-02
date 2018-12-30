d75a84b62fdf98a13e6b1bc80158952fcbe6fbf8
Rabie Shishko <rabie707@gmail.com>
2018-12-30 15:47:28 +0200

    added Get/users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 var { mongoose } = require('./db/mongoose');
 var {User}  = require('./models/user');
 var {Todo}  = require('./models/todo');
-
+var {authenticate} = require('./middleware/authenticate');
 var app = express();
 
 app.use(bodyparser.json());
@@ -113,6 +113,11 @@ app.post('/users', (req, res) => {
     });
 
 });
+
+
+app.get('/users/me', authenticate, (req,res)=>{
+   res.send(req.user);
+})
 
 app.listen(3000,()=>{
 
